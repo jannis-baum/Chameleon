39fd90a4e571436dbed9242f8f8e9f43dbe60da2
Jannis Baum <jannis.baum@gmail.com>
2023-08-09 13:46:40 +0200

    feat: vim highlight commands

--- lib/data.py
+++ lib/data.py
@@ -4,3 +4,12 @@ class Highlight:
         self.bg = bg
         self.deco = deco
         self.ul = ul
+
+    def vim(self, group: str) -> str:
+        return ' '.join([
+            f'highlight {group}',
+            f'ctermfg={self.fg or "none"}',
+            f'ctermbg={self.bg or "none"}',
+            f'cterm={self.deco or "none"}',
+            f'ctermul={self.ul or "none"}',
+        ])
