3515ea478ab0d473a1a14f2cfaaa9ece2ef1f7c0
Jannis Baum <jannis.baum@gmail.com>
2023-08-10 09:50:08 +0200

    refactor: vim hl

--- lib/schemes/vim.py
+++ lib/schemes/vim.py
@@ -1,8 +1,17 @@
 from lib.data import Highlight
 from lib.schemes.all import disclaimer
 
+def vim_hl(hl: Highlight, group: str) -> str:
+    return ' '.join([
+        f'highlight {group}',
+        f'ctermfg={hl.fg or "none"}',
+        f'ctermbg={hl.bg or "none"}',
+        f'cterm={hl.deco or "none"}',
+        f'ctermul={hl.ul or "none"}',
+    ])
+
 def vim_out(header: str | None, group2hl: dict[str, Highlight]) -> str:
     header = f'" {disclaimer}\n\n{header or ""}'
     return '\n'.join([header] + [
-        hl.vim(group) for group, hl in group2hl.items()
+        vim_hl(hl, group) for group, hl in group2hl.items()
     ])
